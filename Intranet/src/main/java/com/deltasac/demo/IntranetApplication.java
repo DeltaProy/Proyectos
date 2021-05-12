b61d9bb331608461ccf2f75dc361767240c27300
Rodrigo Gonzales <rgonzalesm1997@gmail.com>
2021-05-12 18:49:37 -0500

    "test 4"

--- Intranet/src/main/java/com/deltasac/demo/IntranetApplication.java
+++ Intranet/src/main/java/com/deltasac/demo/IntranetApplication.java
@@ -9,7 +9,7 @@ public class IntranetApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IntranetApplication.class, args);
 		
-		//Testeando, al hacer push me pide varias veces las credenciales, hace el push pero no deja de pedirmelas test 3
+		//Testeando, al hacer push me pide varias veces las credenciales, hace el push pero no deja de pedirmelas test 4
 	}
 
 }
