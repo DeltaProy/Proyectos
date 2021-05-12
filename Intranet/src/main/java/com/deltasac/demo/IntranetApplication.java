aeb8989414748384ff35d891ef86878cb4e5b52b
Rodrigo Gonzales <rgonzalesm1997@gmail.com>
2021-05-12 18:53:52 -0500

    fin testeo git

--- Intranet/src/main/java/com/deltasac/demo/IntranetApplication.java
+++ Intranet/src/main/java/com/deltasac/demo/IntranetApplication.java
@@ -8,8 +8,6 @@ public class IntranetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntranetApplication.class, args);
-		
-		//Testeando, al hacer push me pide varias veces las credenciales, hace el push pero no deja de pedirmelas test 4
 	}
 
 }
