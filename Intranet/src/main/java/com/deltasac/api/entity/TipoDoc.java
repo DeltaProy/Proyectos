64760be12146d84a10f346c38c6ed3c9d5d18a68
Rodrigo Gonzales <rgonzalesm1997@gmail.com>
2021-05-14 17:49:27 -0500

    Ultimos cambios

---
+++ Intranet/src/main/java/com/deltasac/api/entity/TipoDoc.java
@@ -0,0 +1,44 @@
+package com.deltasac.api.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="mae_tipos_docs")
+public class TipoDoc {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idtipodoc;
+	private String descortipodoc;
+	private String deslartipodoc;
+	
+	public Integer getIdtipodoc() {
+		return idtipodoc;
+	}
+	public void setIdtipodoc(Integer idtipodoc) {
+		this.idtipodoc = idtipodoc;
+	}
+	public String getDescortipodoc() {
+		return descortipodoc;
+	}
+	public void setDescortipodoc(String descortipodoc) {
+		this.descortipodoc = descortipodoc;
+	}
+	public String getDeslartipodoc() {
+		return deslartipodoc;
+	}
+	public void setDeslartipodoc(String deslartipodoc) {
+		this.deslartipodoc = deslartipodoc;
+	}
+	
+	@Override
+	public String toString() {
+		return "TipoDoc [idtipodoc=" + idtipodoc + ", descortipodoc=" + descortipodoc + ", deslartipodoc="
+				+ deslartipodoc + "]";
+	}
+	
+	
+	
+	
+	
+}
