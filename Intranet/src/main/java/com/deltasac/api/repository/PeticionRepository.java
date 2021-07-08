c12e15ed262b07c8938325fe56e5ef7a2a6da0a6
Rodrigo Gonzales <rgonzalesm1997@gmail.com>
2021-07-08 11:13:54 -0500

    Tareas de peticion

--- Intranet/src/main/java/com/deltasac/api/repository/PeticionRepository.java
+++ Intranet/src/main/java/com/deltasac/api/repository/PeticionRepository.java
@@ -10,12 +10,12 @@ import com.deltasac.api.entity.Peticion;
 
 public interface PeticionRepository extends JpaRepository<Peticion, Integer> {
 
-	@Query("SELECT p FROM Peticion p WHERE p.idarea=?1 AND p.estado<>'FINALIZADA'")
+	@Query("SELECT p FROM Peticion p WHERE p.idarea=?1 AND (p.estado<>'FINALIZADA' AND p.estado<>'CANCELADA')")
 	public List<Peticion> buscarPeticionPorArea(Integer idarea);
 	
-	@Query("SELECT p FROM Peticion p WHERE p.idasignado=?1 AND p.estado<>'FINALIZADA'")
+	@Query("SELECT p FROM Peticion p WHERE p.idasignado=?1 AND (p.estado<>'FINALIZADA' AND p.estado<>'CANCELADA')")
 	public List<Peticion> buscarPeticionPorAsignado(Integer idasignado);
 	
-	@Query("SELECT p FROM Peticion p WHERE p.idusuario=?1 AND p.estado<>'FINALIZADA'")
+	@Query("SELECT p FROM Peticion p WHERE p.idusuario=?1 AND (p.estado<>'FINALIZADA' AND p.estado<>'CANCELADA')")
 	public List<Peticion> buscarPeticionPorUsuario(Integer idusuario);
 }
