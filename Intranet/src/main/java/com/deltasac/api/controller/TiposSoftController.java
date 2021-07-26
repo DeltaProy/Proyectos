8b636712aea04be36b64708f6654a8d56d970244
Rodrigo Gonzales <rgonzalesm1997@gmail.com>
2021-07-26 09:14:04 -0500

    tipos soft controller cors

--- Intranet/src/main/java/com/deltasac/api/controller/TiposSoftController.java
+++ Intranet/src/main/java/com/deltasac/api/controller/TiposSoftController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.deltasac.api.entity.TipoSoft;
 import com.deltasac.api.service.ITiposSoftService;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://173.255.202.95:8080")
 @RestController
 @RequestMapping("/tipossoft")
 public class TiposSoftController {
